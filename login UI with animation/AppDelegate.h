25e2a1e66bdcc62869cbb2a0fa70e16d0f1fd385
zhenghongfeng <835762882@qq.com>
2016-02-15 10:10:30 +0800

    Initial Commit

---
+++ login UI with animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  login UI with animation
+//
+//  Created by zhf on 16/2/15.
+//  Copyright © 2016年 郑洪锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ login UI with animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  login UI with animation
+//
+//  Created by zhf on 16/2/15.
+//  Copyright © 2016年 郑洪锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
